866b6292981fad5365ce0a03843caee97888d72f
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-01-17 16:07:17 +0200

    Structure and AppTheme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import Link from 'next/link'
+import AppTheme from './ui/AppTheme'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,37 +15,40 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ul>
-          <li>
-            <Link href="/cabinet/personal-information">cabinet</Link>
-          </li>
-          <li>
-            <Link href="/cart">cart</Link>
-          </li>
-          <li>
-            <Link href="/about">about</Link>
-          </li>
-          <li>
-            <Link href="/contacts">contacts</Link>
-          </li>
-          <li>
-            <Link href="/reviews">reviews</Link>
-          </li>
-          <li>
-            <Link href="/payment-delivery">payment</Link>
-          </li>
-          <li>
-            <Link href="/support">support</Link>
-          </li> 
-          <li>
-              <Link href="/slug">DynamicPage</Link>
-          </li>
-        </ul>
-        {children}
-      </body>
+      <AppTheme>
+        <body className={inter.className}>
+          <ul>
+            <li>
+              <Link href="/cabinet/personal-information">cabinet</Link>
+            </li>
+            <li>
+              <Link href="/cart">cart</Link>
+            </li>
+            <li>
+              <Link href="/about">about</Link>
+            </li>
+            <li>
+              <Link href="/contacts">contacts</Link>
+            </li>
+            <li>
+              <Link href="/reviews">reviews</Link>
+            </li>
+            <li>
+              <Link href="/payment-delivery">payment</Link>
+            </li>
+            <li>
+              <Link href="/support">support</Link>
+            </li> 
+            <li>
+                <Link href="/slug">DynamicPage</Link>
+            </li>
+          </ul>
+          {children}
+        </body>
+      </AppTheme>
     </html>
   )
 }
