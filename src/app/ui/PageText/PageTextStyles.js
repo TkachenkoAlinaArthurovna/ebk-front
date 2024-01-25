914ff731b4d1db178f45a793e9205260dafa4477
RRedBaron <nedelchevevg@gmail.com>
2024-01-25 20:26:57 +0200

    rebase with dev

---
+++ src/app/ui/PageText/PageTextStyles.js
@@ -0,0 +1,10 @@
+'use client'
+
+import { styled } from '@mui/system';
+
+import { Box, Typography } from '@mui/material';
+
+export const StyledText = styled(Typography)`
+    font-size: 16px;
+    font-weight: 400;
+`;
