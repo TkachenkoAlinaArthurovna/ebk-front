a22274fbf5250c70f027a2dc68dae812d8fa63a0
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-02-05 13:22:47 +0200

    add sliders in product page

--- src/app/ui/ProductPage/ProductPage.jsx
+++ src/app/ui/ProductPage/ProductPage.jsx
@@ -1,12 +1,16 @@
-import Content from '@/app/ui/Content';
 import TabsProductPage from '@/app/ui/ProductPage/TabsProductPage';
+import SlidersProductPage from '@/app/ui/ProductPage/SlidersProductPage';
+import Content from '@/app/ui/Content';
 
 const ProductPage = () => {
   return (
-    <Content>
-      <p>Bread Crumbs</p>
+    <>
+      <Content>
+        <p>Bread Crumbs</p>
+      </Content>
       <TabsProductPage />
-    </Content>
+      <SlidersProductPage />
+    </>
   );
 };
 
--- src/app/ui/ProductPage/TabsProductPage.jsx
+++ src/app/ui/ProductPage/TabsProductPage.jsx
@@ -4,6 +4,7 @@ import { StyledTabs, StyledTab } from '@/app/ui/Tabs/TabsStyled';
 import CustomTabPanel from '@/app/ui/Tabs/CustomTabPanel';
 import { Box } from '@mui/material';
 import { a11yProps } from '@/app/lib/helpers';
+import Content from '@/app/ui/Content';
 import AboutProduct from '@/app/ui/ProductPage/AboutProduct';
 import СharacteristicsProduct from '@/app/ui/ProductPage/СharacteristicsProduct';
 import DescriptionPage from '@/app/ui/ProductPage/DescriptionPage';
@@ -17,28 +18,30 @@ const TabsProductPage = () => {
   };
   return (
     <StyledSection as="section">
-      <Box
-        sx={{ marginBottom: '24px', borderBottom: 1, borderColor: 'divider' }}
-      >
-        <StyledTabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
+      <Content>
+        <Box
+          sx={{ marginBottom: '24px', borderBottom: 1, borderColor: 'divider' }}
         >
-          <StyledTab label="Про товар" {...a11yProps(0)} />
-          <StyledTab label="Характеристики" {...a11yProps(1)} />
-          <StyledTab label="Опис" {...a11yProps(2)} />
-        </StyledTabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        <AboutProduct />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <СharacteristicsProduct />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <DescriptionPage />
-      </CustomTabPanel>
+          <StyledTabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <StyledTab label="Про товар" {...a11yProps(0)} />
+            <StyledTab label="Характеристики" {...a11yProps(1)} />
+            <StyledTab label="Опис" {...a11yProps(2)} />
+          </StyledTabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <AboutProduct />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <СharacteristicsProduct />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          <DescriptionPage />
+        </CustomTabPanel>
+      </Content>
     </StyledSection>
   );
 };
