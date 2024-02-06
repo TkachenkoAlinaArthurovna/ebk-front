1fe4b4ee6630bba374aed8248daab13c8134e169
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-02-06 11:05:51 +0200

    fix cart img and files configuration

--- src/app/ui/CartItem/CartItem.jsx
+++ src/app/ui/CartItem/CartItem.jsx
@@ -6,7 +6,7 @@ import {
   StyledButton,
   StyledButtonGroup,
   StyledImageWrapper,
-  StyledPriceTypography
+  StyledPriceTypography,
 } from '@/app/ui/CartItem/CartItemStyles';
 import { useState } from 'react';
 
@@ -19,14 +19,24 @@ const CartItem = () => {
 
   const handleDecrement = () => {
     setCount((prevCount) => prevCount - 1);
-  }
+  };
 
   return (
     <Box mt={3}>
-      <Box sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+      <Box
+        sx={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}
+      >
         <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
           <StyledImageWrapper>
-            <Image src="/images/city-e-bike.jpg" alt="item-preview" width={60} height={40}></Image>
+            <Image
+              src="/images/city-e-bike.jpg"
+              alt="item-preview"
+              sizes="100%"
+              fill
+              style={{
+                objectFit: 'contain',
+              }}
+            ></Image>
           </StyledImageWrapper>
           <Box>
             <PageText>Велосипед “Ardis Messina”</PageText>
@@ -36,17 +46,32 @@ const CartItem = () => {
 
         <Box sx={{ display: 'flex', alignItems: 'center', gap: '45px' }}>
           <StyledButtonGroup>
-            <StyledButton variant="text" onClick={handleDecrement} disabled={count === 1}>-</StyledButton>
+            <StyledButton
+              variant="text"
+              onClick={handleDecrement}
+              disabled={count === 1}
+            >
+              -
+            </StyledButton>
             <PageText>{count}</PageText>
-            <StyledButton variant="text" onClick={handleIncrement}>+</StyledButton>
+            <StyledButton variant="text" onClick={handleIncrement}>
+              +
+            </StyledButton>
           </StyledButtonGroup>
           <Box>
             <StyledPriceTypography>15 000 ₴</StyledPriceTypography>
             <PageText color={'#DC362E'}>12 999 ₴</PageText>
-          </Box></Box>
-
+          </Box>
+        </Box>
       </Box>
-      <Box sx={{ my: '24px', display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+      <Box
+        sx={{
+          my: '24px',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          width: '100%',
+        }}
+      >
         <StyledButton>
           <DeleteOutlineOutlinedIcon
             sx={{ fontSize: '24px', mr: '8px' }}
