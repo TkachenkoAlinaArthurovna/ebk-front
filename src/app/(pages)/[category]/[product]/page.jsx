67034105efe189321c48efcead4d84a59aeedfd2
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-02-18 12:11:04 +0200

    fix

--- src/app/(pages)/[category]/[product]/page.jsx
+++ src/app/(pages)/[category]/[product]/page.jsx
@@ -1,8 +1,7 @@
 import ProductPage from '@/app/ui/ProductPage';
 import { createLinkProduct } from '@/app/lib/createLinkProduct';
 
-export const dynamicParams = false;
-
+// Функция для получения данных о продуктах с сервера
 async function getProducts() {
   const res = await fetch(`https://stage.eco-bike.com.ua/api/products`, {
     next: { revalidate: 3600 },
@@ -11,6 +10,7 @@ async function getProducts() {
   return data;
 }
 
+// Функция для получения данных о категории продукта по его ID
 async function getCategory(id) {
   const res = await fetch('https://stage.eco-bike.com.ua/api/categories', {
     next: { revalidate: 3600 },
@@ -20,30 +20,7 @@ async function getCategory(id) {
   return category ? createLinkProduct(category.name) : null;
 }
 
-export async function generateStaticParams() {
-  const products = await getProducts();
-  const staticParams = await Promise.all(
-    products.map(async (product) => {
-      const category = await getCategory(product.category);
-      const productLink = createLinkProduct(product.name);
-      return {
-        category: category,
-        product: productLink,
-      };
-    }),
-  );
-  return staticParams;
-}
-
-async function getProductId(name) {
-  const products = await getProducts();
-  const product = products.find(
-    (item) => createLinkProduct(item.name) === name,
-  );
-  const productId = product ? product._id : null;
-  return getProduct(productId);
-}
-
+// Функция для получения данных о продукте по его ID
 async function getProduct(productId) {
   const res = await fetch(
     `https://stage.eco-bike.com.ua/api/products/${productId}`,
@@ -53,8 +30,34 @@ async function getProduct(productId) {
   return data;
 }
 
+// Функция для отображения страницы продукта
 export default async function Product({ params }) {
   const { product } = params;
-  const currentProduct = await getProductId(product);
-  return <ProductPage currentProduct={currentProduct} />;
+
+  try {
+    // Получаем данные о продуктах и ID выбранного продукта
+    const products = await getProducts();
+    const productLink = createLinkProduct(product);
+
+    // Ищем выбранный продукт по его названию
+    const selectedProduct = products.find(
+      (item) => createLinkProduct(item.name) === productLink,
+    );
+
+    if (!selectedProduct) {
+      throw new Error('Продукт не найден');
+    }
+
+    // Получаем данные о категории продукта
+    const categoryLink = await getCategory(selectedProduct.category);
+
+    // Получаем данные о продукте по его ID
+    const currentProduct = await getProduct(selectedProduct._id);
+
+    return <ProductPage currentProduct={currentProduct} />;
+  } catch (error) {
+    console.error('Ошибка загрузки продукта:', error);
+    // Обработка ошибок, например, отображение сообщения об ошибке
+    return <div>Ошибка загрузки продукта</div>;
+  }
 }
