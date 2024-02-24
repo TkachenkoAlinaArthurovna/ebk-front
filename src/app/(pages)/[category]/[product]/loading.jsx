51ec7e1c4e41add43a7406459d95829e837a5a4a
FedorBoz <bozadzhy.f@gmail.com>
2024-02-24 12:00:02 +0200

    skelet

--- src/app/(pages)/[category]/[product]/loading.jsx
+++ src/app/(pages)/[category]/[product]/loading.jsx
@@ -1,18 +1,5 @@
-import React from 'react';
-import Skeleton from '@mui/material/Skeleton';
-import Content from '@/app/ui/Content';
-
+import SkeletonProductPage from '@/app/ui/SkeletonProductPage/SkeletonProductPage';
 const Loading = () => {
-  return (
-    <Content>
-      <Skeleton variant="text" sx={{ fontSize: '2rem', width: '450px' }} />
-      <div style={{display: 'flex', margin: '20px 0 240x 0'}}>
-        <Skeleton variant="rounded" width={200} height={35} />
-        <Skeleton variant="rounded" width={200} height={35} />
-        <Skeleton variant="rounded" width={200} height={35} />
-      </div>
-      <Skeleton sx={{margin: '30px'}} variant="rounded" width={400} height={500} />
-    </Content>
-  );
+  return <SkeletonProductPage />;
 };
 export default Loading;
