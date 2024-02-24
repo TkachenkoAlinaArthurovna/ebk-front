585306c56c27a8d9a6f833452d5de782c152742c
FedorBoz <bozadzhy.f@gmail.com>
2024-02-24 23:23:35 +0200

    skeletons product and cutegory

--- src/app/(pages)/[category]/[product]/loading.jsx
+++ src/app/(pages)/[category]/[product]/loading.jsx
@@ -1,7 +1,14 @@
 import SkeletonProductPage from '@/app/ui/SkeletonProductPage/SkeletonProductPage';
 import React from 'react';
 
+<<<<<<< HEAD
 const LoadingProduct = () => {
   return <SkeletonProductPage />;
 };
 export default LoadingProduct;
+=======
+const Loading = () => {
+  return <SkeletonProductPage />;
+};
+export default Loading;
+>>>>>>> 8b39d317dbacb2626be1d4bbdc8969cf49109da3
--- src/app/(pages)/[category]/[product]/page.jsx
+++ src/app/(pages)/[category]/[product]/page.jsx
@@ -83,6 +83,7 @@ async function getProduct(productId) {
 
 export default async function Product({ params }) {
   const { category, product } = params;
+<<<<<<< HEAD
 
   const partsOfCategory = category.includes('%26')
     ? category.split('%26')
@@ -94,4 +95,8 @@ export default async function Product({ params }) {
       partsOfCategory={partsOfCategory}
     />
   );
+=======
+  const currentProduct = await getProductId(category, product);
+  return <ProductPage currentProduct={currentProduct} />;
+>>>>>>> 8b39d317dbacb2626be1d4bbdc8969cf49109da3
 }
