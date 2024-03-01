689e95c6d29b6d5d1b32322a72c215aa1b395bbd
RRedBaron <nedelchevevg@gmail.com>
2024-03-01 06:00:09 +0200

    split schemas

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -1,14 +1,32 @@
 import * as yup from 'yup';
 
-export const contactDataSchema = yup.object({
-  lastname: yup.string().required("Прізвище обов'язкове"),
-  firstname: yup.string().required("Ім'я обов'язкове"),
-  middlename: yup.string().required("По-батькові обов'язкове"),
+const cyrillicOnlyRegex = /^[а-яА-ЯЁё]+$/;
+
+export const basicContactSchema = yup.object({
+  lastname: yup
+    .string()
+    .matches(cyrillicOnlyRegex, 'Прізвище може містити лише кириличні символи')
+    .required("Прізвище обов'язкове"),
+  firstname: yup
+    .string()
+    .matches(cyrillicOnlyRegex, "Ім'я може містити лише кириличні символи")
+    .required("Ім'я обов'язкове"),
+  middlename: yup
+    .string()
+    .matches(
+      cyrillicOnlyRegex,
+      'По-батькові може містити лише кириличні символи',
+    )
+    .required("По-батькові обов'язкове"),
   phone: yup.string().required("Телефон обов'язковий"),
   email: yup
     .string()
     .email('Невірний формат ел. пошти')
     .required("Ел. пошта обов'язкова"),
+});
+
+export const contactDataSchema = yup.object({
+  ...basicContactSchema.fields,
   delivery: yup.string().required("Спосіб доставки обов'язковий"),
   payment: yup.string().required("Спосіб оплати обов'язковий"),
   comment: yup.string().max(200, 'Максимальна довжина коментаря 200 символів'),
