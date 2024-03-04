d3a4ec228bccbbadd9770692568bdac7e76051d8
FedorBoz <bozadzhy.f@gmail.com>
2024-03-04 22:01:45 +0200

    fixing viewed prod

--- src/app/ui/ProductPage/ProductPage.jsx
+++ src/app/ui/ProductPage/ProductPage.jsx
@@ -1,9 +1,26 @@
+'use client';
+import { useEffect, useState } from 'react';
 import TabsProductPage from '@/app/ui/ProductPage/TabsProductPage';
 import SlidersProductPage from '@/app/ui/ProductPage/SlidersProductPage';
 import Content from '@/app/ui/Content';
 import BreadCrumbsDynamic from '@/app/ui/BreadCrumbsDynamic';
+import ProductsList from '@/app/ui/CategoryPage/CategoryItems/ProductsList';
 
 const ProductPage = ({ currentProduct, partsOfCategory }) => {
+  const [filteredArr, setFilteredArray] = useState([]);
+
+  useEffect(() => {
+    const sessionArr = JSON.parse(sessionStorage.getItem('currentProduct'));
+    if (!sessionArr || sessionArr.length === 0) {
+      return;
+    } else {
+      const filterArr = sessionArr.filter(
+        (obj) => obj._id !== currentProduct._id,
+      );
+      setFilteredArray(filterArr);
+    }
+  }, []);
+  // console.log(filteredArr)
   return (
     <>
       <Content>
@@ -13,7 +30,12 @@ const ProductPage = ({ currentProduct, partsOfCategory }) => {
         />
       </Content>
       <TabsProductPage currentProduct={currentProduct} />
-      <SlidersProductPage currentProduct={currentProduct} />
+      {filteredArr.length > 4 ? (
+        <SlidersProductPage filteredArr={filteredArr} />
+      ) : (
+        // изменить стили для єтого компонента
+        <ProductsList products={filteredArr} />
+      )}
     </>
   );
 };
