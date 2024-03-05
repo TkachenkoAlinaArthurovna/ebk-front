c464f4a004797ccdf331889a4632511c6a2dc2a7
FedorBoz <bozadzhy.f@gmail.com>
2024-03-05 13:06:25 +0200

    fixed viewed products

--- src/app/ui/ProductPage/ProductPage.jsx
+++ src/app/ui/ProductPage/ProductPage.jsx
@@ -5,6 +5,10 @@ import SlidersProductPage from '@/app/ui/ProductPage/SlidersProductPage';
 import Content from '@/app/ui/Content';
 import BreadCrumbsDynamic from '@/app/ui/BreadCrumbsDynamic';
 import ProductsList from '@/app/ui/CategoryPage/CategoryItems/ProductsList';
+import { StyledSection } from '@/app/ui/ProductPage/ProductPageStyles';
+import { Box } from '@mui/material';
+import PageTitle from '@/app/ui/PageTitle';
+import ViewedList from '@/app/ui/ProductPage/ViewedList';
 
 const ProductPage = ({ currentProduct, partsOfCategory }) => {
   const [filteredArr, setFilteredArray] = useState([]);
@@ -30,11 +34,29 @@ const ProductPage = ({ currentProduct, partsOfCategory }) => {
         />
       </Content>
       <TabsProductPage currentProduct={currentProduct} />
-      {filteredArr.length > 4 ? (
+
+      {filteredArr.length > 0 && filteredArr.length <= 4 && (
+        <StyledSection as="section">
+          <Content>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <Box sx={{ marginBottom: '24px' }}>
+                <PageTitle>Переглянуті товари</PageTitle>
+              </Box>
+              <Box>
+                <ViewedList products={filteredArr} />
+              </Box>
+            </Box>
+          </Content>
+        </StyledSection>
+      )}
+
+      {filteredArr.length > 4 && (
         <SlidersProductPage filteredArr={filteredArr} />
-      ) : (
-        // изменить стили для єтого компонента
-        <ProductsList products={filteredArr} />
       )}
     </>
   );
