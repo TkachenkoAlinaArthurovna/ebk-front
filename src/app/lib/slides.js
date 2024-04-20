44ed024e6524bc4a6323d19461d69e9b344b8bbf
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-04-20 08:52:42 +0300

    fix url

--- src/app/lib/slides.js
+++ src/app/lib/slides.js
@@ -1,10 +1,10 @@
 export const slides = [
   {
     picture: '/images/bike_banner_1.jpg',
-    link: 'http://localhost:3000/miski-elektrovelosypedy&page=2/elektrovelosyped-electric-cubic-26-500w-10ah-aliuminiievyi',
+    link: 'https://eco-bike.com.ua/miski-elektrovelosypedy&page=2/elektrovelosyped-electric-cubic-26-500w-10ah-aliuminiievyi',
   },
   {
     picture: '/images/bike_banner_1.jpg',
-    link: 'http://localhost:3000/miski-elektrovelosypedy&page=2/elektrovelosyped-electric-cubic-26-500w-10ah-aliuminiievyi',
+    link: 'https://eco-bike.com.ua/miski-elektrovelosypedy&page=2/elektrovelosyped-electric-cubic-26-500w-10ah-aliuminiievyi',
   },
 ];
