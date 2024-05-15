2f006c51bd5e1a8c4f2aa0e42e7406d62d2bb065
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-05-15 12:11:21 +0300

    fixed slide

--- src/app/lib/slides.js
+++ src/app/lib/slides.js
@@ -4,7 +4,7 @@ export const slides = [
     link: 'https://eco-bike.com.ua/miski-elektrovelosypedy&page=2/elektrovelosyped-electric-cubic-26-500w-10ah-aliuminiievyi',
   },
   {
-    picture: '/images/bike_banner_2.jpg',
+    picture: '/images/banner_2.jpg',
     link: 'https://eco-bike.com.ua/hirski-elektrovelosypedy/hirskyi-elektrovelosyped-boost-1000w-13-ah',
   },
 ];
